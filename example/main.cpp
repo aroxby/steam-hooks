2a8ce4f12acab464d2479306595d524f778f20ee
Andy Roxby <aroxby@users.noreply.github.com>
2026-06-06 01:41:45 -0400

    Hook the target function!

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,6 @@
 #include <windows.h>
 
 int main(int argc, char *argv[]) {
-    MessageBoxA(nullptr, "Startup", "Example", MB_OK);
-    MessageBoxA(nullptr, "Shutdown", "Example", MB_OK);
+    MessageBoxA(nullptr, "This is from the original application.", "Example", MB_OK);
     return 0;
 }
--- injected/main.cpp
+++ injected/main.cpp
@@ -1,12 +1,41 @@
+#include <iostream>
 #include <windows.h>
+
+#include "MinHook.h"
+
+using namespace std;
+
+typedef int(WINAPI *MESSAGEBOXA)(HWND, LPCSTR, LPCSTR, UINT);
+MESSAGEBOXA OriginalMessageBoxA = nullptr;
+
+int WINAPI DetourMessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType) {
+    return OriginalMessageBoxA(hWnd, "DLL Injected!", lpCaption, uType);
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     switch (fdwReason) {
     case DLL_PROCESS_ATTACH:
-        MessageBoxA(NULL, "Hello from the injected DLL!", "DLL Injection", MB_OK);
+        if (MH_Initialize() != MH_OK) {
+            cerr << "Failed to initialize MinHook." << std::endl;
+            return FALSE;
+        }
+
+        if (MH_CreateHook((LPVOID)&MessageBoxA, (LPVOID)&DetourMessageBoxA, (LPVOID *)&OriginalMessageBoxA) != MH_OK) {
+            cerr << "Failed to create hook." << std::endl;
+            MH_Uninitialize();
+            return FALSE;
+        }
+
+        if (MH_EnableHook((LPVOID)&MessageBoxA) != MH_OK) {
+            cerr << "Failed to enable hook." << std::endl;
+            MH_Uninitialize();
+            return FALSE;
+        }
         break;
 
     case DLL_PROCESS_DETACH:
+        MH_DisableHook((LPVOID)&MessageBoxA);
+        MH_Uninitialize();
         break;
     }
     return TRUE;
